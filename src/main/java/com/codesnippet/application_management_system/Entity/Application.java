965ac2bb6fd350f43690857b99a69cb1b80ca1ac
Rushi Baravkar <rushibaravkar1012@gmail.com>
2025-12-03 12:44:46 +0530

    updated

--- src/main/java/com/codesnippet/application_management_system/Entity/Application.java
+++ src/main/java/com/codesnippet/application_management_system/Entity/Application.java
@@ -16,4 +16,36 @@ public class Application {
     @ManyToOne
     @JoinColumn(name = "applicantId", nullable = false)
     private Applicant applicant;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Applicant getApplicant() {
+        return applicant;
+    }
+
+    public void setApplicant(Applicant applicant) {
+        this.applicant = applicant;
+    }
 }
