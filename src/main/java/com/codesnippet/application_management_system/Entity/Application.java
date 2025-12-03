7132c14b25dc5f56ac0dab1e32110e8009877a6b
Rushi Baravkar <rushibaravkar1012@gmail.com>
2025-12-03 12:39:58 +0530

    updated

---
+++ src/main/java/com/codesnippet/application_management_system/Entity/Application.java
@@ -0,0 +1,19 @@
+package com.codesnippet.application_management_system.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Application {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String status;
+
+    private String position;
+
+    @OneToMany
+    @JoinColumn(name = "applicantId", nullable = false)
+    private Applicant applicant;
+}
